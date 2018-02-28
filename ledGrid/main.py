14b754ebb1b1f1fc521049001cf35c31086855cc
Daniel O Byrne <daniel.obyrne@ucdconnect.ie>
2018-02-28 22:10:30 +0000

    Got the file_exists def working with link to txt file given in the question sheet. Commented out the other test and functions to get this working first

--- ledGrid/main.py
+++ ledGrid/main.py
@@ -1,4 +1,7 @@
 '''Main file'''
+import sys 
+import urllib.request
+
 
 def main(filename,N):
     lights = LightTester(N)
@@ -7,7 +10,14 @@ def main(filename,N):
         lights.apply(cmd)
         
     print("# occupied", light.count())
-    
+
+def file_exists(filename):
+    '''Checks if a file exists in the local address or a network address that is given'''
+    if filename.startswith('http://'):
+        returned_file = urllib.request.urlopen(filename)
+    else:
+        returned_file = open(filename, 'r')
+    return returned_file
 
 
 
@@ -15,35 +25,44 @@ class LightTester():
     
     lights = None
     
-    def.__init__(self,N):
-        self.lights = [[False]*size for _ in range(size)]
+    def __init__(self,N):
+        ''' LED grid has N x N number of lights. All lights start as off'''
+        self.lights = [[False]*N for _ in range(N)]
+        self.size = N
         
-    def apply(self,cmd):
-        if cmd is 'switch on':
-            #do something to switch it off
-        elif cmd is 'switch off':
-            #do something to switch it on
-        elif
-            #insert other if statements for other scenarios
-    
-     def file_exists(filename):
-    '''Checks if a file exists in the local address or a network address'''
-
-
-    def ignoring_commands(filename):
-    '''Ignore any commands which are not "turn on", "turn off" or "switch" '''
-    
-    def turn_on():
-    '''turns on lights given coordinates, if they are currently off'''
-
-     def turn_off():
-     '''turns on lights given coordinates, if they are currently off'''
-    
-    
-    def within_region(filename):
-    '''Checks if coordinates given are outside grid, ignores points outside grid and lights up parts inside'''
-           
+   
         
-    def count(self):
-        return count
-        #this should count the number of lights that are on
+        
+    
+            
+
+#===============================================================================
+#         
+#     def apply(self,cmd):
+#         if cmd is 'switch on':
+#             #do something to switch it off
+#         #elif cmd is 'switch off':
+#             #do something to switch it on
+#        # elif
+#             #insert other if statements for other scenarios
+#     
+#      
+# 
+#     def ignoring_commands(filename):
+#     '''Ignore any commands which are not "turn on", "turn off" or "switch" '''
+#     
+#     def turn_on():
+#     '''turns on lights given coordinates, if they are currently off'''
+# 
+#      def turn_off():
+#      '''turns on lights given coordinates, if they are currently off'''
+#     
+#     
+#     def within_region(filename):
+#     '''Checks if coordinates given are outside grid, ignores points outside grid and lights up parts inside'''
+#            
+#         
+#     def count(self):
+#         return count
+#         #this should count the number of lights that are on
+#===============================================================================
--- tests/tests.py
+++ tests/tests.py
@@ -1,18 +1,21 @@
 '''This is the test file'''
 
+import sys
 
+from ledGrid.main import *
 
 def test_file_exists():
+    test_file = "http://claritytrec.ucd.ie/~alawlor/comp30670/input_assign3_d.txt"
+    assert file_exists(test_file) != None
     
     
-def test_ignoring_commands():
+#def test_ignoring_commands():
     
-    
-def test_turn_on():
+#def test_turn_on():
     
 
-def test_turn_off():
+#def test_turn_off():
 
     
-def test_within_region(filename):
+#def test_within_region(filename):
        
