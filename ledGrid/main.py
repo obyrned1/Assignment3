3d9a84fde99b2e78e120bb6c45c9c4b57df765ee
Daniel O Byrne <daniel.obyrne@ucdconnect.ie>
2018-02-28 15:58:11 +0000

    Set up structure given by lectures in main.py to get a sense of what is required

--- ledGrid/main.py
+++ ledGrid/main.py
@@ -1,1 +1,30 @@
-# This is the main.py file
+'''Main file'''
+
+def main(filename,N):
+    lights = LightTester(N)
+    instructions = parse_file(filename)
+    for cmd in instruction:
+        lights.apply(cmd)
+        
+    print("# occupied", light.count())
+    
+
+
+class LightTester():
+    
+    lights = None
+    
+    def.__init__(self,N):
+        self.lights = [[False]*size for _ in range(size)]
+        
+    def apply(self,cmd):
+        if cmd is 'switch on':
+            #do something to switch it off
+        elif cmd is 'switch off':
+            #do something to switch it on
+        elif
+            #insert other if statements for other scenarios
+        
+    def count(self):
+        return count
+        #this should count the number of lights that are on
